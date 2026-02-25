ad6fa57ef0833627ebddeafe51d0089483b258f3
Nantenaina29 <elhadari04@gmail.com>
2026-02-25 08:45:27 +0300

    Modification api et axiosClient avec env

--- src/api.js
+++ src/api.js
@@ -1,8 +1,12 @@
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-ong-qarl.onrender.com", // Ny URL-ny Render fa tsy localhost intsony
+  baseURL: "https://backend-ong-qarl.onrender.com",
   withCredentials: true,
+  headers: {
+    "X-Requested-With": "XMLHttpRequest", // Tena ilaina ho an'ny Laravel
+  }
 });
 
 export default api;
--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,16 +1,18 @@
+
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'https://backend-ong-qarl.onrender.com/api',
+  baseURL: 'https://backend-ong-qarl.onrender.com/api', 
   withCredentials: true,
   headers: {
     "Accept": "application/json",
     "Content-Type": "application/json",
+    "X-Requested-With": "XMLHttpRequest", 
   },
 });
 
 axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); 
+  const token = localStorage.getItem("ACCESS_TOKEN"); 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
