822bf58a3705d99232d0155ade6b77e7927cada1
Nantenaina29 <elhadari04@gmail.com>
2026-02-27 22:38:34 +0300

    Fix:Modification  axiosClient

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -2,12 +2,22 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: 'https://backend-ong-w5t4.onrender.com/api',
-  timeout: 15000,
-  withCredentials: true, // <--- TSY MAINTSY MISY IO satria supports_credentials = true
+  timeout: 15000, // Ampitomboina ho 30s satria mi-sleep ny Render (Cold Start)
   headers: { 
     "Content-Type": "application/json",
     "Accept": "application/json"
   }
+});
+
+// TSY MAINTSY: Ampitaina isaky ny requete ny Token avy ao amin'ny localStorage
+axiosClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem('ACCESS_TOKEN');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default axiosClient;
