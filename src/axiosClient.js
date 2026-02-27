b8ee7d21c3e8ca9ddaf749ebea5acac06ef17b9d
Nantenaina29 <elhadari04@gmail.com>
2026-02-27 07:37:11 +0300

    Update URL axiosClient

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'https://backend-ong-qarl.onrender.com/api',
+  baseURL: 'https://backend-ong-w5t4.onrender.com/api',
   timeout: 15000,
   withCredentials: true, // <--- TSY MAINTSY MISY IO satria supports_credentials = true
   headers: { 
